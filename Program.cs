36ce4fcd669f7ac8c1241628f73ddf8787ff0c53
Taesuuu <82026757+Taesuuu@users.noreply.github.com>
2024-03-12 22:22:39 +0900

    [Taesuuu] 프로그래머스 2개 이하로 다른 비트

--- Program.cs
+++ Program.cs
@@ -7,55 +7,80 @@ namespace Taesuuu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Solution("ULURRDLLU"));
-        }
-        public static int Solution(string dirs) {
-            
-            int answer = 0;
-            // 가로 세로 11 씩 가지는 좌표 평면 배열 생성
-            int[,,,] check = new int[11, 11, 11, 11];
-            // 0.0 부터 5.5 까지 기떄문에 x,y 를 5로 초기화
-            int x = 5, y = 5;
-
-            for (int i = -1; ++i < dirs.Length;)
-            {
-                // 이전의 값과 비교해서 지나온 길인지 아닌지 체크하기 위해 데이터 저장
-                int oldX = x, oldY = y;
-                // 1개씩 빼와서 체크
-                char direction = dirs[i];
-                // 문제에 조건대로 움직이도록 세팅
-                if (direction == 'U') ++y;
-                if (direction == 'D') --y;
-                if (direction == 'R') ++x;
-                if (direction == 'L') --x;
-                // 10보다 크다면 0으로 초기화
-                x = MaxOrMin(x);
-                y = MaxOrMin(y);
-                // 이전 값과 다른지 체크
-                if (oldX != x || oldY != y)
-                // 이전값과 다르고 좌표의 값이 0인지 체크 후 
-                    if (check[oldX, oldY, x, y] == 0)
-                    {
-                        // 해당 배열에 위치 만큼 이동하도록 설정
-                        check[oldX, oldY, x, y] = 1;
-                        check[x, y, oldX, oldY] = 1;
-                        // 움직임 횟수 증가
-                        ++answer;
-                    }
+            long[] numbers = new long[] {2,7};
+            long[] temp = solution(numbers);
+            foreach(long i in temp) {
+                Console.WriteLine(i);
             }
-
-            return answer;
         }
 
-        static int MaxOrMin(int num) {
-            if (num >= 10) {
-                return 10;
+        public static long[] solution(long[] numbers) {
+            List<long> answer = new List<long>();
+            foreach (long number in numbers) {
+                answer.Add(FindNextNumber(number));
             }
-            if(num <= 0) {
-                return 0;
+            return answer.ToArray();
+        }
+
+        static long FindNextNumber(long number) {
+            long temp = 0;
+            // 입력 받은 수가 짝수면 + 1 하면 2개 비트 이하로 차이
+            if (number % 2 == 0)
+                temp = number + 1;
+            else {
+                // 홀수 일 경우는 마지막 수를 0으로 변경하고 
+                // 7이기에 111
+                string s = Convert.ToString(number, 2);
+                int idx = s.LastIndexOf("0");
+                // 0에 대한 인덱스 체크
+                if (idx == -1){
+                    s = "10" + s.Substring(1);
+                }
+                else {
+                    // 구한 인덱스에 0을 추가하고 나머지 2개 제외한 비트 0으로 변환
+                    s = s.Substring(0, idx) + "10" + s.Substring(idx + 2);
+                }
+                temp =   Convert.ToInt64(s,2);
+                
             }
-            return num;
+            return temp;
         }
 
     }
 }
+
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+
+// public class Solution {
+//       public long[] solution(long[] numbers) {
+//         List<long> answer = new List<long>();
+//         foreach (long number in numbers) {
+//             answer.Add(FindNextNumber(number));
+//         }
+//         return answer.ToArray();
+//     }
+
+//     static long FindNextNumber(long number) {
+//         long temp = number;
+//         while (true) {
+//             long nextNumber = ++temp;
+//             int bitDifference = BitDifference(number, nextNumber);
+//             if (2 >= bitDifference) {
+//                 return nextNumber;
+//             }
+//         }
+//     }
+//     모든 비트를 비교했기 떄문에 로직은 맞지만 long에 큰 수가 오게 되면 시간 초과 발생
+//     static int BitDifference(long a, long b) {
+//         long xor = a ^ b;
+//         int count = 0;
+//         while (xor != 0) {
+//             count += (int)(xor & 1);
+//             xor >>= 1;
+//         }
+//         return count;
+//     }
+
+// }
